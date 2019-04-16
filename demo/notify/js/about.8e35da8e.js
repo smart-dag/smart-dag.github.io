23ad42887095d0836840947ab79204ad8c3ea973
cr4fun <cr4fun@outlook.com>
2019-04-16 16:20:21 +0800

    demo

---
+++ demo/notify/js/about.8e35da8e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var c=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("Test only")])])}],a=n("17cc"),l={},u=Object(a["a"])(l,c,s,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=about.8e35da8e.js.map
