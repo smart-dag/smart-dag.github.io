e0ec294d679baa17a6beac8790cf582813915a2b
cr4fun <cr4fun@outlook.com>
2019-04-24 10:57:54 +0800

    notify

---
+++ demo/notify/js/about.e028162c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("Test only")])])}],l=n("2877"),u={},c=Object(l["a"])(u,s,a,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=about.e028162c.js.map
